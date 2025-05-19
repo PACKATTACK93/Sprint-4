fc3768e75481b0573c0a90ba3c19ad1408ca3e64
PACKATTACK93 <brandonsully93@gmail.com>
2025-05-19 14:53:58 -0600

    app.py file rename

--- Car-Sales app.py
+++ Car-Sales app.py
@@ -8,3 +8,13 @@ import altair as alt
 from PIL import Image
 
 # Set page configuration
+st.set_page_config(
+    page_title="Car Sales Ads",
+    page_icon="🚗",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+# Load data
+@st.cache_data
+def load_data():
+    return pd.read_csv("vehicles_ads.csv")
